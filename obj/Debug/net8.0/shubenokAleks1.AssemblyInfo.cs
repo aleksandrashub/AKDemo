2eb791068aefbceb5ddcf0a91f89f5cd9147ace2
dmitiry <you@example.com>
2024-09-27 16:12:47 +0300

    first commit

--- obj/Debug/net8.0/shubenokAleks1.AssemblyInfo.cs
+++ obj/Debug/net8.0/shubenokAleks1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shubenokAleks1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8669d7594a936d8319c70b106c77668b89d5248b")]
 [assembly: System.Reflection.AssemblyProductAttribute("shubenokAleks1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shubenokAleks1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
